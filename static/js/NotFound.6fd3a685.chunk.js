b9b7d08d9b3db0fa7f684813f282edcaa7ab2cc7
Vadoc1337 <19071999v@gmail.com>
2024-04-06 23:52:30 +0700

    Updates

---
+++ static/js/NotFound.6fd3a685.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[810],{7150:(p,s,a)=>{a.r(s),a.d(s,{default:()=>c});a(5043);var e=a(8428),u=a(579);const c=()=>(0,u.jsx)(e.T6,{})}}]);
+//# sourceMappingURL=NotFound.6fd3a685.chunk.js.map
