3e1e4954d38536880295c567e8a551878e049553
Vadoc1337 <19071999v@gmail.com>
2024-04-19 23:04:12 +0700

    Added comments and refactored MainLayout

--- src/hooks/useDynamicStyles.ts
+++ src/hooks/useDynamicStyles.ts
@@ -1,60 +1,135 @@
-// * Будущая функция для MainLayout
-// import {useLocation} from "react-router-dom";
-// import {useSelector} from "react-redux";
-// import {selectFilter} from "../redux/slices/filterSlice";
-// import {selectCart} from "../redux/slices/cartSlice";
-// import useWindowWidth from "./useWindowWidth";
-// import useWindowHeight from "./useWindowHeight";
-// import calculateScaleAndMargin from "../utils/marginScaleFunc";
-// import React from "react";
-//
-//
-//
-//
-// let CartEmptyMargin;
-// let wideScreen;
-// const LAPTOP_WIDTH = 1455;
-// const SMARTPHONE_WIDTH = 768;
-// const ASPECT_RATIO_THRESHOLD = 1.72;
-// const CONTENT_HEIGHT_THRESHOLD = 748;
-// const isCart = location.pathname === `/Sushi-Heaven/cart`;
-//
-// export default function useDynamicStyles () {
-//     const location = useLocation();
-//
-//     const { searchValue } = useSelector(selectFilter);
-//     const { totalPrice } = useSelector(selectCart);
-//
-//     const windowWidth = useWindowWidth();
-//     const windowHeight = useWindowHeight();
-//     const { scale, marginTopBottom } = calculateScaleAndMargin(windowHeight);
-//
-//     const shouldApplyTransform =
-//         windowWidth > LAPTOP_WIDTH &&
-//         !searchValue &&
-//         contentHeight <= CONTENT_HEIGHT_THRESHOLD &&
-//         !isCart;
-//
-//
-//     const contentRef = React.useRef<HTMLDivElement>(null);
-//     const [contentHeight, setContentHeight] = React.useState(0);
-//
-//     if (shouldApplyTransform && wideScreen) {
-//         return {
-//             transform: `scale(${scale})`,
-//             margin: `${marginTopBottom}px auto`,
-//             transition: "all 0.2s ease",
-//         };
-//     } else if (shouldApplyTransform && !wideScreen) {
-//         return {
-//             margin: `${marginTopBottom}px auto`,
-//             transition: "all 0.2s ease",
-//         };
-//     } else if (CartEmptyMargin && windowWidth > SMARTPHONE_WIDTH) {
-//         return {
-//             margin: `8vh auto`,
-//             transition: "all 0.2s ease",
-//         };
-//     }
-//     return {}; // Default case when no styles need to be applied
-// };
+/* !Фрагмент кода представляет собой функцию useDynamicStyles, которая вычисляет и возвращает объект динамического стиля на основе различных условий.
+? Входные данные
+Функция не принимает явных входных данных, но она опирается на следующие переменные и перехватчики:
+location: объект текущего местоположения из библиотеки react-router-dom
+isCart: логическое значение, указывающее, является ли текущее местоположение Корзиной
+searchValue: строка, представляющая текущее значение поиска из redux store
+items: массив, представляющий товары в Корзине (значение из redux store)
+windowWidth: число представляющий текущую ширину окна Пользователя
+windowHeight: число, представляющее текущую высоту окна Пользователя
+cartEmpty: логическое значение, указывающее, пуста ли Корзина
+scale: число, представляющее вычисленное значение масштаба увелечение элементов
+marginTopBottom: число, представляющее вычисленное значение поля верхнего и нижнего отступа (марджинов)
+
+* Сначала функция проверяет, является ли текущее местоположение страницей - Корзиной.
+Затем она извлекает значение для поиска и товаров из redux store с помощью функции useSelector.
+Затем она вычисляет ширину и высоту окна, используя кастомный хук useWindowWidth и useWindowHeight.
+Функция определяет, соответствует ли текущая ширина окна определенным условиям для применения стиля преобразования.
+На основе иных различных условий (ширина компьютера, планшета, смартфона, соотношении сторон и т.д.) функция возвращает объект динамического стиля с различными свойствами и значениями. */
+
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectCart } from "../redux/slices/cartSlice";
+import { selectFilter } from "../redux/slices/filterSlice";
+
+import calculateScaleAndMargin from "../utils/marginScaleFunc";
+import useWindowHeight from "../hooks/useWindowHeight";
+import useWindowWidth from "../hooks/useWindowWidth";
+
+const LAPTOP_WIDTH = 1455;
+const MIN_TABLET_WIDTH = 768;
+const MIN_SMARTPHONE_WIDTH = 585;
+const ASPECT_RATIO_THRESHOLD = 1.72;
+
+export default function useDynamicStyles() {
+  const location = useLocation();
+  const isCart = location.pathname === `/Sushi-Heaven/cart`;
+
+  const { searchValue } = useSelector(selectFilter);
+  const { items } = useSelector(selectCart);
+
+  const windowWidth = useWindowWidth();
+  const windowHeight = useWindowHeight();
+  const cartEmpty = isCart && items.length === 0;
+  const wideScreen =
+    windowWidth / windowHeight >= ASPECT_RATIO_THRESHOLD &&
+    windowWidth > LAPTOP_WIDTH;
+
+  const { scale, marginTopBottom } = calculateScaleAndMargin(windowHeight);
+
+  const shouldApplyTransform = windowWidth > LAPTOP_WIDTH && !searchValue;
+
+  if (shouldApplyTransform && wideScreen && !isCart && windowHeight >= 500) {
+    return {
+      transform: `scale(${scale})`,
+      margin: `${marginTopBottom}px auto`,
+      transition: "all 0.2s ease",
+    };
+  } else if (shouldApplyTransform && !wideScreen) {
+    return {
+      margin: `${marginTopBottom}px auto`,
+      transition: "all 0.2s ease",
+    };
+  } else if (shouldApplyTransform && isCart && items.length <= 4) {
+    return {
+      transform: `scale(${scale})`,
+      margin: `${marginTopBottom}px auto`,
+      transition: "all 0.2s ease",
+    };
+  } else if (shouldApplyTransform && isCart && items.length > 4) {
+    return {
+      transition: "all 0.2s ease",
+    };
+  } else if (windowWidth >= MIN_TABLET_WIDTH && !cartEmpty && !isCart) {
+    return {
+      margin: `5vh auto 3vh`,
+      width: `calc(100% - 50px)`,
+      transition: "all 0.2s ease",
+    };
+  } else if (
+    windowWidth >= MIN_TABLET_WIDTH &&
+    !cartEmpty &&
+    items.length > 2
+  ) {
+    return {
+      margin: `5vh auto 3vh`,
+      width: `calc(100% - 50px)`,
+      transition: "all 0.2s ease",
+    };
+  } else if (
+    windowWidth >= MIN_TABLET_WIDTH &&
+    !cartEmpty &&
+    items.length <= 2 &&
+    windowHeight >= 805
+  ) {
+    return {
+      width: `calc(100% - 50px)`,
+      margin: `${marginTopBottom}px auto`,
+      transition: "all 0.2s ease",
+    };
+  } else if (
+    cartEmpty &&
+    windowWidth >= MIN_TABLET_WIDTH &&
+    windowHeight >= 805
+  ) {
+    return {
+      margin: `${marginTopBottom}px auto`,
+      transition: "all 0.2s ease",
+    };
+  } else if (windowWidth <= MIN_SMARTPHONE_WIDTH && !isCart) {
+    return {
+      margin: `2vh 5px`,
+      transition: "all 0.2s ease",
+    };
+  } else if (
+    windowWidth <= MIN_SMARTPHONE_WIDTH &&
+    isCart &&
+    windowHeight >= 900
+  ) {
+    return {
+      margin: `${marginTopBottom}px 5px`,
+      transition: "all 0.2s ease",
+    };
+  } else if (
+    windowWidth <= MIN_SMARTPHONE_WIDTH &&
+    isCart &&
+    windowHeight <= 900
+  ) {
+    return {
+      margin: `30px 5px`,
+      transition: "all 0.2s ease",
+    };
+  }
+  return {};
+}
--- src/utils/marginScaleFunc.ts
+++ src/utils/marginScaleFunc.ts
@@ -1,13 +1,26 @@
 import { IMarginScaleData } from "../data/declarations";
 
+/* * В данном коде реализована функция calculateScaleAndMargin, которая вычисляет масштаб и отступы для заданной высоты экрана, используя данные из массива scaleMarginData.Основные моменты
+Массив данных scaleMarginData: Это массив объектов, каждый из которых содержит поля height, scale и marginTopBottom. Эти данные используются для вычисления масштаба и отступов в зависимости от высоты.
+
+Функция binarySearch: Эта функция выполняет бинарный поиск по высоте в массиве scaleMarginData и возвращает индекс элемента, который соответствует или ближайший к заданной высоте.
+
+Функция interpolate: Функция для интерполяции (вычисления промежуточного значения) между двумя числами на основе параметра t, который представляет собой долю между ними.
+
+Функция calculateScaleAndMargin -
+1) проверяет, не пуст ли массив scaleMarginData.
+2) Обрабатывает крайние случаи, когда заданная высота меньше минимальной или больше максимальной высоты в массиве.
+3) Использует binarySearch для нахождения подходящих индексов для интерполяции.
+4) Вычисляет масштаб и отступы, используя interpolate для интерполяции между ближайшими значениями. */
+
 const scaleMarginData: IMarginScaleData[] = [
-  { height: 500, scale: 0.62, marginTopBottom: -142},
-  { height: 600, scale: 0.75, marginTopBottom: -90},
+  { height: 500, scale: 0.62, marginTopBottom: -142 },
+  { height: 600, scale: 0.75, marginTopBottom: -90 },
   { height: 750, scale: 0.9, marginTopBottom: -20 },
-  { height: 800, scale: 1, marginTopBottom: 10},
-  { height: 900, scale: 1.1, marginTopBottom: 60},
-  { height: 1080, scale: 1.23, marginTopBottom: 145.745},
-  { height: 1440, scale: 1.54, marginTopBottom: 317.236},
+  { height: 800, scale: 1, marginTopBottom: 10 },
+  { height: 900, scale: 1.1, marginTopBottom: 60 },
+  { height: 1080, scale: 1.23, marginTopBottom: 145.745 },
+  { height: 1440, scale: 1.54, marginTopBottom: 317.236 },
   { height: 2160, scale: 2.2, marginTopBottom: 683 },
 ];
 
@@ -54,7 +67,11 @@ export default function calculateScaleAndMargin(height: number): {
 
   const t = (height - lowerData.height) / (upperData.height - lowerData.height);
   const scale = interpolate(lowerData.scale, upperData.scale, t);
-  const marginTopBottom = interpolate(lowerData.marginTopBottom, upperData.marginTopBottom, t);
+  const marginTopBottom = interpolate(
+    lowerData.marginTopBottom,
+    upperData.marginTopBottom,
+    t,
+  );
 
   return { scale, marginTopBottom };
 }
